dc1dcd354413a4e529f70c4589974070b907f1b6
jowsing <jowsing169@gmail.com>
2025-02-06 10:50:21 +0800

    Bug fixed.

--- Sources/UITableView+.swift
+++ Sources/UITableView+.swift
@@ -30,36 +30,36 @@ public extension UITableView {
     }
     
     func register<T: UITableViewCell>(_ cellClass: T.Type, extra: String = "") {
-        let reuseIdentifier = cellClass.reuseIdentifier
-        register(cellClass, forCellReuseIdentifier: reuseIdentifier)
+        let reuseIdentifier = cellClass.reuseIdentifier + extra
+        self.register(cellClass, forCellReuseIdentifier: reuseIdentifier)
     }
 
     func register<T: UITableViewHeaderFooterView>(_ viewClass: T.Type, extra: String = "") {
-        let reuseIdentifier = viewClass.reuseIdentifier
-        register(viewClass, forHeaderFooterViewReuseIdentifier: reuseIdentifier)
+        let reuseIdentifier = viewClass.reuseIdentifier + extra
+        self.register(viewClass, forHeaderFooterViewReuseIdentifier: reuseIdentifier)
     }
 
     func dequeueReusableCell<T: UITableViewCell>(_ cellClass: T.Type, extra: String = "") -> T {
-        let cell = dequeueReusableCell(withIdentifier: cellClass.reuseIdentifier + extra)
-        guard let cellType = cell as? T else {
+        let reuseIdentifier = cellClass.reuseIdentifier + extra
+        guard let cell = dequeueReusableCell(withIdentifier: reuseIdentifier) as? T else {
             fatalError("Unable to dequeue:\(cellClass)")
         }
-        return cellType
+        return cell
     }
 
     func dequeueReusableCell<T: UITableViewCell>(_ cellClass: T.Type, indexPath: IndexPath, extra: String = "") -> T {
-        let cell = dequeueReusableCell(withIdentifier: cellClass.reuseIdentifier + extra, for: indexPath)
-        guard let cellType = cell as? T else {
+        let reuseIdentifier = cellClass.reuseIdentifier + extra
+        guard let cell = self.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? T else {
             fatalError("Unable to dequeue:\(cellClass)")
         }
-        return cellType
+        return cell
     }
 
     func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(_ viewClass: T.Type, extra: String = "") -> T {
-        let view = dequeueReusableHeaderFooterView(withIdentifier: viewClass.reuseIdentifier + extra)
-        guard let viewType = view as? T else {
+        let reuseIdentifier = viewClass.reuseIdentifier + extra
+        guard let view = self.dequeueReusableHeaderFooterView(withIdentifier: reuseIdentifier) as? T else {
             fatalError("Unable to dequeue:\(viewClass)")
         }
-        return viewType
+        return view
     }
 }
